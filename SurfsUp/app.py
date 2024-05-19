7914fe8c517a201573ca88609e3300be8ecb24e9
gfoley15 <garrett.foley15@gmail.com>
2024-05-19 11:46:20 -0500

    building flask api and routes

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,99 @@
 # Import the dependencies.
-
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
 
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
+Base.prepare(autoload_with=engine)
 
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(bind=engine)
 
 #################################################
 # Flask Setup
 #################################################
 
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Find the most recent date in the data set.
+    most_recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+
+    # Design a query to retrieve the last 12 months of precipitation data and plot the results. 
+    # Starting from the most recent data point in the database. 
+    most_recent_date = most_recent_date[0]
+    most_recent_date = dt.datetime.strptime(most_recent_date, '%Y-%m-%d').date()
+
+    # Calculate the date one year from the last date in data set.
+    one_year_date = most_recent_date - dt.timedelta(days=365)
+
+    """Return a list of precipitation data including date and precipitation amount"""
+    # Query precipitation from Measurement table
+    prcp_query = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= one_year_date)
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    prcp_data = []
+    for date, prcp, in prcp_query:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+        prcp_data.append(prcp_dict)
+
+    return jsonify(prcp_data)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all stations"""
+    # Query all passengers
+    station_query = session.query(Station.station).distinct().all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    all_stations = list(np.ravel(station_query))
+
+    return jsonify(all_stations)
+
+if __name__ == '__main__':
+    app.run(debug=True)
