1d5570d51d2a8a7d74f1d2230c413bcbd7085da2
gfoley15 <garrett.foley15@gmail.com>
2024-05-19 17:07:34 -0500

    API routes code, dynamic date route

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -43,7 +43,9 @@ def welcome():
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
     )
 
 @app.route("/api/v1.0/precipitation")
@@ -69,9 +71,9 @@ def precipitation():
 
     session.close()
 
-    # Create a dictionary from the row data and append to a list of all_passengers
+    # Create a dictionary from the row data and append to a list
     prcp_data = []
-    for date, prcp, in prcp_query:
+    for date, prcp in prcp_query:
         prcp_dict = {}
         prcp_dict["date"] = date
         prcp_dict["prcp"] = prcp
@@ -85,7 +87,7 @@ def stations():
     session = Session(engine)
 
     """Return a list of all stations"""
-    # Query all passengers
+    # Query all stations
     station_query = session.query(Station.station).distinct().all()
 
     session.close()
@@ -94,6 +96,77 @@ def stations():
     all_stations = list(np.ravel(station_query))
 
     return jsonify(all_stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all temperature obserations for the last 12 months"""
+    # Using the most active station
+    # Query the last 12 months of temperature observation data for most active station
+    lastyr_temps_query = session.query(Measurement.date, Measurement.tobs).\
+    filter(Measurement.station == 'USC00519281').\
+    filter(Measurement.date >= one_year_date).\
+    filter(Measurement.date <= most_recent_date).all()
+    
+    session.close()
+
+    # Create a dictionary from the row data and append to a list
+    temps_data = []
+    for date, tobs in lastyr_temps_query:
+        tobs_dict = {}
+        tobs_dict["date"] = date
+        tobs_dict["tobs"] = tobs
+        temps_data.append(tobs_dict)
+
+    return jsonify(temps_data)
+
+@app.route("/api/v1.0/<start>")
+def start_date(start):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of MIN, MAX, and AVG temperature obserations for all the dates greater than or equal to the start date"""
+   start_date_query = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
+        filter(Measurment.date >= start).all()
+    
+    session.close()
+
+    # Create a dictionary from the row data and append to a list
+    start_temps_data = []
+    for row in start_date_query:
+        start_date_temps_dict = {}
+        low_t, high_t, avg_t = row
+        start_date_temps_dict["low_t"] = low_t
+        start_date_temps_dict["high_t"] = high_t
+        start_date_temps_dict["avg_t"] = avg_t
+        start_temps_data.append(start_date_temps_dict)
+
+    return jsonify(start_temps_data)
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end_date(start, end):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of MIN, MAX, and AVG temperature obserations for the dates from the start date to the end date, inclusive."""
+   start_end_date_query = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list
+    start_end_temps_data = []
+    for row in start_end_date_query:
+        start_end_date_temps_dict = {}
+        low_t, high_t, avg_t = row
+        start_end_date_temps_dict["low_t"] = low_t
+        start_end_date_temps_dict["high_t"] = high_t
+        start_end_date_temps_dict["avg_t"] = avg_t
+        start_temps_data.append(start_date_temps_dict)
+
+    return jsonify(start_end_temps_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
